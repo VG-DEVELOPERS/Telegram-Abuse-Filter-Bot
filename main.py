0273a84a52f2978f6edfa8c94e3d449105cc03ca
VG-DEVELOPERS <152973395+VG-DEVELOPERS@users.noreply.github.com>
2025-03-20 21:48:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def main():
     app.add_handler(CommandHandler("admin", admin_control))
     app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, handle_new_group))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    app.add_handler(MessageHandler(filters.Sticker.ALL | filters.VIDEO.ALL, detect_nsfw))
+    app.add_handler(MessageHandler(filters.Sticker.ALL | filters.VIDEO, detect_nsfw))
     app.add_handler(CallbackQueryHandler(button_handler))
     
     app.run_polling()
